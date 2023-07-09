f7ad9cdfb359ada43b78e180fe8b07ad42ef53ba
Vlad <arniofficial1@gmail.com>
2023-07-09 21:27:10 +0300

    add redirects

---
+++ src/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import MainScreen from './components/MainScreen/MainScreen'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      
+      <FirstScreen />
+    <HistoryScreen/>
+    <TravelScreen/>
+    <GalleryScreen/>
+    <ContactsScreen/>
+    </div>
+  )
+}
+
+export default App
+
+
