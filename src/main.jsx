fbe1760f9575b8d1e2ae8f048f3bf4d1dc52a47b
Vlad <arniofficial1@gmail.com>
2023-07-09 21:37:13 +0300

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FirstScreen from './assets/components/FirstScreen/FirstScreen.jsx'
-import  './main.module.css'
-import HistoryScreen from './assets/components/HistoryScreen/HistoryScreen.jsx'
-import TravelScreen from './assets/components/TravelScreen/TravelScreen.jsx'
-import GalleryScreen from './assets/components/GalleryScreen/GalleryScreen.jsx'
-import ContactsScreen from './assets/components/ContactsScreen/ContactsScreen.jsx'
-import { Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FirstScreen from "./assets/components/FirstScreen/FirstScreen.jsx";
+import "./main.module.css";
+import HistoryScreen from "./assets/components/HistoryScreen/HistoryScreen.jsx";
+import TravelScreen from "./assets/components/TravelScreen/TravelScreen.jsx";
+import GalleryScreen from "./assets/components/GalleryScreen/GalleryScreen.jsx";
+import ContactsScreen from "./assets/components/ContactsScreen/ContactsScreen.jsx";
+import { Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstScreen />
-    <HistoryScreen/>
-    <TravelScreen/>
-    <GalleryScreen/>
-    <ContactsScreen/>
-   
-  </React.StrictMode>,
-)
+    <Router>
+      <FirstScreen />
+      <HistoryScreen />
+      <TravelScreen />
+      <GalleryScreen />
+      <ContactsScreen />
+    </Router>
+  </React.StrictMode>
+);
