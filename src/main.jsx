198f2d5f84b9146dea280d8ddee0cab9bc5dcc53
Vlad <arniofficial1@gmail.com>
2023-07-12 19:21:51 +0300

    add deploy worflows

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FirstScreen from "./assets/components/FirstScreen/FirstScreen.jsx";
 import "./main.module.css";
-import HistoryScreen from "./assets/components/HistoryScreen/HistoryScreen.jsx";
-import TravelScreen from "./assets/components/TravelScreen/TravelScreen.jsx";
-import GalleryScreen from "./assets/components/GalleryScreen/GalleryScreen.jsx";
-
-import App from "./assets/components/App.jsx";
-
-import Router from './assets/components/Router.jsx';
+import Router from "./assets/components/Router.jsx";
 
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router />
