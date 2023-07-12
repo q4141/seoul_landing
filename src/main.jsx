272fc1d554a5bbd6aadf453b7aca1bed7f5905d1
Vlad <arniofficial1@gmail.com>
2023-07-12 19:32:46 +0300

    123

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.module.css";
-import Router from "./assets/components/Router.jsx";
 import HistoryScreen from "./assets/components/HistoryScreen/HistoryScreen";
-
+import FirstScreen from "./assets/components/FirstScreen/FirstScreen";
+import TravelScreen from "./assets/components/TravelScreen/TravelScreen";
+import GalleryScreen from "./assets/components/GalleryScreen/GalleryScreen";
+import ContactsScreen from "./assets/components/ContactsScreen/ContactsScreen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <HistoryScreen/>
+    <FirstScreen />
+    <HistoryScreen />
+    <TravelScreen />
+    <GalleryScreen />
+    <ContactsScreen />
   </React.StrictMode>
 );
