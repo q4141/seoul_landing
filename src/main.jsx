b5c7ea308421f578f51f78cfba285a47837787fe
Vlad <arniofficial1@gmail.com>
2023-07-12 19:26:19 +0300

    123

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.module.css";
 import Router from "./assets/components/Router.jsx";
+import HistoryScreen from "./assets/components/HistoryScreen/HistoryScreen";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router />
+   <HistoryScreen/>
   </React.StrictMode>
 );
